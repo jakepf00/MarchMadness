508cbd4a1e9f65113470768c2dee14e85f74a4c4
Jacob Pfeifer <jakepf00@gmail.com>
2024-03-15 14:37:26 -0500

    Base layer class

---
+++ Layer.py
@@ -0,0 +1,13 @@
+# Base class
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    # Computes the output Y of a layer for a given input X
+    def forward_propagation(self, input):
+        raise NotImplementedError
+
+    # Computes dE/dX for a given dE/dY (and update parameters if any)
+    def backwarg_propagation(self, output_error, learning_rate):
+        raise NotImplementedError
