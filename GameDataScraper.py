54792497bb1a6241c8e8526bd746e0e9ebfce0b3
Jacob Pfeifer <jakepf00@gmail.com>
2024-03-19 11:34:23 -0500

    Complete game data scraper

--- GameDataScraper.py
+++ GameDataScraper.py
@@ -1,4 +1,7 @@
+from bs4 import BeautifulSoup
 import datetime
+import pathlib
+import requests
 
 def generateUrls2024():
     begin_2024 = datetime.datetime(2023, 11, 6)
@@ -7,5 +10,29 @@ def generateUrls2024():
     dates_2024 = [begin_2024 + datetime.timedelta(days=x) for x in range(delta_2024.days)]
     return ["https://www.ncaa.com/scoreboard/basketball-men/d1/" + x.strftime("%Y") + "/" + x.strftime("%m") + "/" + x.strftime("%d") + "/all-conf" for x in dates_2024]
 
-games2024 = generateUrls2024()
-print(games2024)
+games_2024 = generateUrls2024()
+
+pathlib.Path("Games/").mkdir(parents=True, exist_ok=True)
+gameFile = open("Games/2024.csv", "w")
+
+for game_url in games_2024:
+    r = requests.get(game_url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    games_list = soup.find('div', class_ = "gamePod_content-pod_container").find_all(class_ = "gamePod gamePod-type-game status-final")
+
+    for game in games_list:
+        team1 = game.find_all('li')[0]
+        team2 = game.find_all('li')[1]
+
+        team1_name = team1.find('span', class_ = "gamePod-game-team-name").text
+        team1_score = team1.find('span', class_ = "gamePod-game-team-score").text
+        team2_name = team2.find('span', class_ = "gamePod-game-team-name").text
+        team2_score = team2.find('span', class_ = "gamePod-game-team-score").text
+
+        point_diff = str(int(team1_score) - int(team2_score))
+
+        gameFile.write(team1_name.replace(" ", "_") + "," + team2_name.replace(" ", "_") + "," + point_diff + "\n")
+
+
+
